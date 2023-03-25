cd1d9b3106a94a77ac3018f19821dd69eef563f7
지오현 <jihkyuoo@naver.com>
2023-03-25 09:38:37 +0900

    env 환경설정
    1. @nestjs/config 설치
    2. cross-env 설치
    3. .env.dev .env.test 파일 생성
    4. env gitignore 에 추가
    5. 앱 모듈에 ConfigModule 추가 후 옵션설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,16 +2,34 @@ import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver } from '@nestjs/apollo';
 import { RestaurantsModule } from './restaurants/restaurants.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: process.env.NODE_ENV === 'dev' ? '.env.dev' : '.env.test',
+    }),
     GraphQLModule.forRoot({
       driver: ApolloDriver,
       autoSchemaFile: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'jiohyeon',
+      password: '12345',
+      database: 'nuber-eats',
+      entities: [],
+      synchronize: true,
+      logging: true,
     }),
     RestaurantsModule,
   ],
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule {
+}
