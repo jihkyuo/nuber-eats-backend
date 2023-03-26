d31ebfcc7274bd83f9f8efa0c53447b6132be643
지오현 <jihkyuoo@naver.com>
2023-03-26 20:34:25 +0900

    Joi => env 유효성 검사
    1. 모듈 설정에 validationSchema에 유효성 검사
    2. Joi를 통해 환경변수들의 타입과 유효성 옵션 설정
    3. 유효성 검사가 맞지 않으면 에러가 남

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import * as Joi from 'joi';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver } from '@nestjs/apollo';
 import { RestaurantsModule } from './restaurants/restaurants.module';
@@ -11,6 +12,14 @@ import { ConfigModule } from '@nestjs/config';
       isGlobal: true,
       envFilePath: process.env.NODE_ENV === 'dev' ? '.env.dev' : '.env.test',
       ignoreEnvFile: process.env.NODE_ENV === 'prod', // prod 환경일때 ConfigModule 이 환경병수 파일을 무시한다
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string().valid('dev', 'prod').required(),
+        DB_HOST: Joi.string().required(),
+        DB_PORT: Joi.string().required(),
+        DB_USERNAME: Joi.string().required(),
+        DB_PASSWORD: Joi.string().required(),
+        DB_NAME: Joi.string().required(),
+      }),
     }),
     GraphQLModule.forRoot({
       driver: ApolloDriver,
