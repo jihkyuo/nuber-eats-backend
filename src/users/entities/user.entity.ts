979ff55c7072cb7dd3c678253c518f6652e6aa03
지오현 <jihkyuoo@naver.com>
2023-03-28 10:17:20 +0900

    Users resolver, service 설정

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,8 +1,11 @@
 import { Column, Entity } from 'typeorm';
 import { CoreEntity } from '../../common/entities/common.entity';
+import { InputType, ObjectType } from '@nestjs/graphql';
 
 type UserRole = 'client' | 'owner' | 'delivery';
 
+@InputType({ isAbstract: true })
+@ObjectType()
 @Entity()
 export class User extends CoreEntity {
 
