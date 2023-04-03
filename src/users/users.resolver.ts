ed91555165dd72af2bea2092e56d3e608eaccc58
지오현 <jihkyuoo@naver.com>
2023-04-03 23:58:17 +0900

    userProfile dto 정의 & 버그 픽스

--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -6,15 +6,11 @@ import { LoginInput, LoginOutput } from './dtos/login.dto';
 import { UseGuards } from '@nestjs/common';
 import { AuthGuard } from '../auth/auth.guard';
 import { AuthUser } from '../auth/auth-user.decorator';
+import { UserProfileInput, UserProfileOutput } from './dtos/user-profile.dto';
 
 @Resolver(of => User)
 export class UsersResolver {
   constructor(private readonly usersService: UsersService) {
-  }
-
-  @Query(returns => Boolean)
-  hi() {
-    return true;
   }
 
   @Mutation(returns => CreateAccountOutput)
@@ -45,5 +41,25 @@ export class UsersResolver {
   @UseGuards(AuthGuard)
   me(@AuthUser() authUser: User) {
     return authUser;
+  }
+
+  @UseGuards(AuthGuard)
+  @Query(returns => UserProfileOutput)
+  async userProfile(@Args() userProfileInput: UserProfileInput): Promise<UserProfileOutput> {
+    try {
+      const user = await this.usersService.findById(userProfileInput.userId);
+      if (!user) {
+        throw Error();
+      }
+      return {
+        ok: true,
+        user,
+      };
+    } catch (error) {
+      return {
+        error: 'User Not Found',
+        ok: false,
+      };
+    }
   }
 }
