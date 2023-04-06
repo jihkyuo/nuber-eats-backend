df7b4211fd8868fa3f5275df4791b3a91ceeb3f8
지오현 <jihkyuoo@naver.com>
2023-04-07 02:05:42 +0900

    Mail module 생성

---
+++ src/mail/mail.module.ts
@@ -0,0 +1,16 @@
+import { DynamicModule, Module } from '@nestjs/common';
+import { CONFIG_OPTIONS } from '../common/common.constants';
+import { MailModuleOptions } from './mail.interface';
+
+@Module({})
+export class MailModule {
+  static forRoot(options: MailModuleOptions): DynamicModule {
+    return {
+      module: MailModule,
+      providers: [
+        { provide: CONFIG_OPTIONS, useValue: options },
+      ],
+      exports: [],
+    };
+  }
+}
