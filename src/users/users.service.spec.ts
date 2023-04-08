d014ea25f84acaf89cf24c15934b3d8c297bbf39
지오현 <jihkyuoo@naver.com>
2023-04-08 21:36:49 +0900

    userService(createAccount) verification save TEST

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -27,6 +27,7 @@ type MockRepository<T = any> = Partial<Record<keyof Repository<T>, jest.Mock>>
 describe('UserService', () => {
   let service: UsersService;
   let usersRepository: MockRepository<User>;
+  let verificationRepository: MockRepository<Verification>;
 
   beforeAll(async () => {
     const module = await Test.createTestingModule({
@@ -52,6 +53,7 @@ describe('UserService', () => {
     }).compile();
     service = module.get<UsersService>(UsersService);
     usersRepository = module.get(getRepositoryToken(User));
+    verificationRepository = module.get(getRepositoryToken(Verification));
   });
 
 
@@ -79,8 +81,13 @@ describe('UserService', () => {
     });
 
     it('새로운 유저 생성', async () => {
+      /** for user test */
       usersRepository.findOne.mockResolvedValue(undefined); // user 가 없다고 속임
       usersRepository.create.mockReturnValue(createAccountArgs);
+
+      /** for verification test */
+      usersRepository.save.mockResolvedValue(createAccountArgs)
+      verificationRepository.create.mockReturnValue(createAccountArgs)
       await service.createAccount(createAccountArgs);
 
       expect(usersRepository.create).toHaveBeenCalledTimes(1); // 단 한번 호출될거라 기대
@@ -88,6 +95,14 @@ describe('UserService', () => {
 
       expect(usersRepository.save).toHaveBeenCalledTimes(1);
       expect(usersRepository.save).toHaveBeenCalledWith(createAccountArgs);
+
+      expect(verificationRepository.create).toHaveBeenCalledTimes(1);
+      expect(verificationRepository.create).toHaveBeenCalledWith({
+        user: createAccountArgs,
+      });
+
+      expect(verificationRepository.save).toHaveBeenCalledTimes(1);
+      expect(verificationRepository.save).toHaveBeenCalledWith(createAccountArgs);
     });
   });
 
