87123022269bd7fdd6ac37ae0f91bf88c7e81cfd
지오현 <jihkyuoo@naver.com>
2023-04-18 23:34:26 +0900

    e2e test 세팅
    - jest-e2e.json은 e2e 테스트를 설정하는 곳이고, 경로 설정이 추가로 필요하기 때문에, package.json의
     "moduleNameMapper": {
          "^src/(.*)$": "<rootDir>$1"
        }, 부분을 가져와 경로를 수정한다

--- test/users.e2e-spec.ts
+++ test/users.e2e-spec.ts
@@ -3,22 +3,18 @@ import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
 
-describe('AppController (e2e)', () => {
+describe('UserModule (e2e)', () => {
   let app: INestApplication;
 
   beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
+    const module: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
-    app = moduleFixture.createNestApplication();
+    app = module.createNestApplication();
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
-  });
+
+
 });
