6523efdd5c7927d4823a99b41a866f43a567874b
지오현 <jihkyuoo@naver.com>
2023-04-18 23:41:45 +0900

    e2e test 세팅
    - app module에 보면, Joi 설정이 test 환경일 때의 세팅이 필요하다.
    npm run test로 실행하기 때문에 NODE_ENV는 test가 추가로 필요하고,
    envFilePath에서 .env.test 파일을 경로로 찾기 때문에
    .env.test에서 환경변수 설정이 필요하다

--- test/users.e2e-spec.ts
+++ test/users.e2e-spec.ts
@@ -1,6 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
 
 describe('UserModule (e2e)', () => {
@@ -14,7 +13,12 @@ describe('UserModule (e2e)', () => {
     app = module.createNestApplication();
     await app.init();
   });
-
+  it.todo('me')
+  it.todo('userProfile')
+  it.todo('createAccount')
+  it.todo('login')
+  it.todo('editProfile')
+  it.todo('verifyEmail')
 
 
 });
